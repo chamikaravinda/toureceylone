c77b33ed3e701d763b746287935e399cdde7b629
“w.deshan” <w.deshan@directfn.com>
2019-04-10 12:13:50 +0530

    initial Commit of home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Background from './img/img.jpg'
+
+var sectionStyle = {
+    width: "100%",
+    height: "1000px",
+    backgroundImage: `url(${Background})`
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <section style={ sectionStyle }>
+            <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+                <div className="container">
+                    <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+                    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                            data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        Menu
+                        <i className="fas fa-bars"></i>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                        <ul className="navbar-nav text-uppercase ml-auto">
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#services">Services</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#about">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#team">Team</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <p>HIII</p>
+        </section>
     );
   }
 }
