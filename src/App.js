6fb6bd19d711b9c4a7bd88b207c32cf345212134
“w.deshan” <w.deshan@directfn.com>
2019-04-10 13:55:34 +0530

    Home Router Commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
 import React, {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import HomeComponent from "./component/common_home.component.js"
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+
+
 class App extends Component {
     render() {
         return (
-            <div>
+            <Router>
                 <div>
-                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                        <div className="container">
-                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul className="navbar-nav mr-auto">
-                                    <li className="nav-item active">
-                                        <a className="nav-link" href="#">Home<span
-                                            className="sr-only">(current)</span></a>
-                                    </li>
-                                    <li className="nav-item active">
-                                        <a className="nav-link" href="#">Ticketing<span
-                                            className="sr-only">(current)</span></a>
-                                    </li>
-                                    <li className="nav-item active">
-                                        <a className="nav-link" href="#">Service Providers<span
-                                            className="sr-only">(current)</span></a>
-                                    </li>
-                                </ul>
-                                <form className="form-inline my-2 my-lg-0">
-                                    <input className="form-control mr-sm-2" type="search" placeholder="Search"
-                                           aria-label="Search"/>
-                                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search
-                                    </button>
-                                </form>
+                    <div>
+                        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                            <div className="container">
+                                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                    <ul className="navbar-nav mr-auto">
+                                        <li className="nav-item active">
+                                            <a className="nav-link" href="#">Home<span
+                                                className="sr-only">(current)</span></a>
+                                        </li>
+                                        <li className="nav-item active">
+                                            <a className="nav-link" href="#">Ticketing<span
+                                                className="sr-only">(current)</span></a>
+                                        </li>
+                                        <li className="nav-item active">
+                                            <a className="nav-link" href="#">Service Providers<span
+                                                className="sr-only">(current)</span></a>
+                                        </li>
+                                    </ul>
+                                    <form className="form-inline my-2 my-lg-0">
+                                        <input className="form-control mr-sm-2" type="search" placeholder="Search"
+                                               aria-label="Search"/>
+                                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search
+                                        </button>
+                                    </form>
+                                </div>
                             </div>
-                        </div>
-                    </nav>
+                        </nav>
+                    </div>
+                    <Route path="/" exact component={HomeComponent}/>
                 </div>
-            </div>
+            </Router>
 
         );
     }
