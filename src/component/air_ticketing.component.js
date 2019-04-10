9c37763f591612e5bc9b3f78a3aebfd603d20784
“w.deshan” <w.deshan@directfn.com>
2019-04-10 17:14:35 +0530

    air ticket commit

--- src/component/air_ticketing.component.js
+++ src/component/air_ticketing.component.js
@@ -1,15 +1,15 @@
 import React, {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Background from "../img/Sri-Lanka-4K.jpg";
-import img1 from "../img/category_ancient cities.jpg";
-import img2 from "../img/category_hillCountry.jpg";
-import img3 from "../img/category_SeaSide.jpg";
-import img4 from "../img/category_wildLife.jpg";
+import Background from "../img/superman.jpg";
+import img1 from "../img/srilankanair.jpg";
+import img2 from "../img/malayasiaair.jpg";
+import img3 from "../img/quantas.jpg";
+import img4 from "../img/flydubai.jpg";
 
 
 var sectionStyle = {
     width: "100%",
-    height: "100%",
+    height: "1000px",
     backgroundImage: `url(${Background})`,
     backgroundRepeat: "none",
     backgroundPosition: "center",
@@ -18,28 +18,36 @@ var sectionStyle = {
 
 var divOverlayStyle = {
     zindex: "10",
-    backgroundColor: "rgba(0,0,0,0.5)"
+    backgroundColor: "rgba(0,0,0,0.5)",
+    height: "800px",
+    textAlign: "center"
 
 
 };
+
+var imageStyle = {
+    width: "200",
+    height: "120px",
+}
 
 
 export default class AirTicketing extends Component {
     render() {
         return (
             <div style={sectionStyle}>
-                <div className="container">
+                <br/><br/>
+                <div className="container-fluid" style={divOverlayStyle}>
                     <br/><br/>
-                    <h2>WHERE YOU WANT TO GO</h2>
+                    <h2 style={{color:"white"}}><b>Lets Fly Mate</b></h2>
                     <br/>
                     <div className="row">
                         <div className="col-md-10 offset-md-1">
                             <div className="card-deck">
                                 <div className="card">
                                     <img className="card-img-top" src={img1}
-                                         alt="Card image cap"/>
+                                         alt="Card image cap" style={imageStyle}/>
                                     <div className="card-body">
-                                        <h5 className="card-title">Hill Country</h5>
+                                        <h5 className="card-title">Sri Lankan AirLines</h5>
                                         <button type="button" className="btn  btn-sm  btn-primary">Choose
                                         </button>
                                     </div>
@@ -48,10 +56,10 @@ export default class AirTicketing extends Component {
                                     </div>
                                 </div>
                                 <div className="card">
-                                    <img className="card-img-top" src="img/category_SeaSide.jpg"
-                                         alt="Card image cap"/>
+                                    <img className="card-img-top" src={img2}
+                                         alt="Card image cap" style={imageStyle}/>
                                     <div className="card-body">
-                                        <h5 className="card-title">Sandy Beaches</h5>
+                                        <h5 className="card-title">Malaysian Air</h5>
                                         <button type="button" className="btn btn-sm  btn-primary">Choose
                                         </button>
 
@@ -61,10 +69,10 @@ export default class AirTicketing extends Component {
                                     </div>
                                 </div>
                                 <div className="card">
-                                    <img className="card-img-top" src="img/category_wildLife.jpg"
-                                         alt="Card image cap"/>
+                                    <img className="card-img-top" src={img3}
+                                         alt="Card image cap" style={imageStyle}/>
                                     <div className="card-body">
-                                        <h5 className="card-title">Explore Wild Life</h5>
+                                        <h5 className="card-title">Quantas</h5>
                                         <button type="button" className="btn btn-sm  btn-primary">Choose
                                         </button>
 
@@ -74,10 +82,10 @@ export default class AirTicketing extends Component {
                                     </div>
                                 </div>
                                 <div className="card">
-                                    <img className="card-img-top" src="img/category_ancient%20cities.jpg"
-                                         alt="Card image cap"/>
+                                    <img className="card-img-top" src={img4}
+                                         alt="Card image cap" style={imageStyle}/>
                                     <div className="card-body">
-                                        <h5 className="card-title">Ancient Cities </h5>
+                                        <h5 className="card-title">Fly Dubai</h5>
                                         <button type="button" className="btn btn-sm btn-primary">Choose
                                         </button>
 
