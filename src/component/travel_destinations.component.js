f7d61f6aa63621e3e112f348c252f203d7a3c9a8
Chamika Ravinda <chamikaravinda@gmail.com>
2019-04-11 15:57:13 +0530

    travel destination added

--- src/component/travel_destinations.component.js
+++ src/component/travel_destinations.component.js
@@ -32,7 +32,66 @@ export default class  TravelDestination extends Component {
             <div style={sectionStyle}>
                 <div className="container-fluid" style={{width: 1000, height: 1000}}>
                     <div className="container">
-                        <h1 className="display-1 align-content-center">Places to visit</h1>
+                        <br/><br/>
+                        <h2>WHERE YOU WANT TO GO</h2>
+                        <br/>
+                            <div className="row">
+                                <div className="col-md-10 offset-md-1">
+                                    <div className="card-deck">
+                                        <div className="card">
+                                            <img className="card-img-top" src="img/category_hillCountry.jpg"
+                                                 alt="Card image cap"/>
+                                                <div className="card-body">
+                                                    <h5 className="card-title">Hill Country</h5>
+                                                    <button type="button" className="btn  btn-sm  btn-primary">Choose
+                                                    </button>
+                                                </div>
+                                                <div className="card-footer">
+                                                    <small className="text-muted">Last updated 3 mins ago</small>
+                                                </div>
+                                        </div>
+                                        <div className="card">
+                                            <img className="card-img-top" src="img/category_SeaSide.jpg"
+                                                 alt="Card image cap"/>
+                                                <div className="card-body">
+                                                    <h5 className="card-title">Sandy Beaches</h5>
+                                                    <button type="button" className="btn btn-sm  btn-primary">Choose
+                                                    </button>
+
+                                                </div>
+                                                <div className="card-footer">
+                                                    <small className="text-muted">Last updated 3 mins ago</small>
+                                                </div>
+                                        </div>
+                                        <div className="card">
+                                            <img className="card-img-top" src="img/category_wildLife.jpg"
+                                                 alt="Card image cap"/>
+                                                <div className="card-body">
+                                                    <h5 className="card-title">Explore Wild Life</h5>
+                                                    <button type="button" className="btn btn-sm  btn-primary">Choose
+                                                    </button>
+
+                                                </div>
+                                                <div className="card-footer">
+                                                    <small className="text-muted">Last updated 3 mins ago</small>
+                                                </div>
+                                        </div>
+                                        <div className="card">
+                                            <img className="card-img-top" src="img/category_ancient%20cities.jpg"
+                                                 alt="Card image cap"/>
+                                                <div className="card-body">
+                                                    <h5 className="card-title">Ancient Cities </h5>
+                                                    <button type="button" className="btn btn-sm btn-primary">Choose
+                                                    </button>
+
+                                                </div>
+                                                <div className="card-footer">
+                                                    <small className="text-muted">Last updated 3 mins ago</small>
+                                                </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                     </div>
                 </div>
             </div>
